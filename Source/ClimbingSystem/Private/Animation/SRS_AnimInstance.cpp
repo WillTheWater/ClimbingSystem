8047dd7bb056f87e0a02b4ed7da742708f0592b6
WillTheWater <willthewater@protonmail.com>
2025-09-13 10:10:07 +0200

    Add SRS_AnimInstance for climbing animation logic
    Introduces SRS_AnimInstance C++ class and header to handle climbing animation state, including ground speed, air speed, movement, and falling checks. Updates related assets to integrate the new animation instance into the climbing system.

---
+++ Source/ClimbingSystem/Private/Animation/SRS_AnimInstance.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/SRS_AnimInstance.h"
+
+#include "ClimbingSystem/ClimbingSystemCharacter.h"
+#include "ClimbingSystem/Public/SRS_MovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void USRS_AnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ClimbingSystemCharacter = Cast<AClimbingSystemCharacter>(TryGetPawnOwner());
+	if (ClimbingSystemCharacter)
+	{
+		CustomMovementComponent = ClimbingSystemCharacter->GetCustomMovementComponent();
+	}
+}
+
+void USRS_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!CustomMovementComponent || !ClimbingSystemCharacter) { return; }
+	GetGroundSpeed();
+	GetAirSpeed();
+	GetShouldMove();
+	GetIsFalling();
+}
+
+void USRS_AnimInstance::GetGroundSpeed()
+{
+	GroundSpeed = UKismetMathLibrary::VSizeXY(ClimbingSystemCharacter->GetVelocity());
+}
+
+void USRS_AnimInstance::GetAirSpeed()
+{
+	AirSpeed = ClimbingSystemCharacter->GetVelocity().Z;
+}
+
+void USRS_AnimInstance::GetShouldMove()
+{
+	bShouldMove =
+		CustomMovementComponent->GetCurrentAcceleration().Size() > 0.f &&
+		GroundSpeed > 5.f && !bIsFalling;
+}
+
+void USRS_AnimInstance::GetIsFalling()
+{
+	bIsFalling = CustomMovementComponent->IsFalling();
+}
---
+++ Source/ClimbingSystem/Public/Animation/SRS_AnimInstance.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SRS_AnimInstance.generated.h"
+
+
+class USRS_MovementComponent;
+class AClimbingSystemCharacter;
+
+UCLASS()
+class CLIMBINGSYSTEM_API USRS_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+
+private:
+	UPROPERTY()
+	AClimbingSystemCharacter* ClimbingSystemCharacter;
+
+	UPROPERTY()
+	USRS_MovementComponent* CustomMovementComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing", meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed { 0.0f };
+
+	void GetGroundSpeed();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing", meta = (AllowPrivateAccess = "true"))
+	float AirSpeed { 0.0f };
+
+	void GetAirSpeed();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing", meta = (AllowPrivateAccess = "true"))
+	bool bShouldMove { false };
+
+	void GetShouldMove();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing", meta = (AllowPrivateAccess = "true"))
+	bool bIsFalling { false };
+
+	void GetIsFalling();
+};
